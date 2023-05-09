112bb1297879e8837a738110c0bf3f4a0af3ec28
matteovcc <matteovcc99@gmail.com>
2023-05-09 18:04:46 +0200

    aggiustamenti

--- src/Esercizi/Main1.java
+++ src/Esercizi/Main1.java
@@ -7,18 +7,11 @@ import java.util.Set;
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Set<String> setParoleDistinte = new HashSet<>();
 		Set<String> setParoleDuplicate = new HashSet<>();
 		Scanner in = new Scanner(System.in);
 		System.out.print("Inserisci N  elementi da inserire nei set: ");
 		int n = in.nextInt();
-
-//		ArrayList<String> s = new ArrayList<>[n];
-//		Iterator<String> i = s.iterator();
-//		while (i.hasNext()) {
-//			System.out.println(i.next() + "Inserisci parola");
-//		}
 
 		String[] s = new String[n];
 		for (int i = 0; i < n; i++) {
@@ -26,10 +19,8 @@ public class Main1 {
 			s[i] = in.next();
 		}
 		for (String parola : s) {
-			if (setParoleDistinte.contains(parola)) {
+			if (!setParoleDistinte.add(parola)) {
 				setParoleDuplicate.add(parola);
-			} else {
-				setParoleDistinte.add(parola);
 			}
 		}
 		System.out.println("set parole duplicate:" + setParoleDuplicate);
