ec937f5b8e260c48d3539670cc5a78f80897dc3e
matteovcc <matteovcc99@gmail.com>
2023-05-09 15:12:13 +0200

    esercizio 1

--- src/Esercizi/Main1.java
+++ src/Esercizi/Main1.java
@@ -8,16 +8,23 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Set<String> setParole = new HashSet<>(); // set distinte
+		Set<String> setParoleDuplicate = new HashSet<>(); // set duplicate
 		Scanner in = new Scanner(System.in);
 		System.out.print("Inserisci N  elementi da inserire nei set: ");
 		int n = in.nextInt();
+
+//		ArrayList<String> s = new ArrayList<>[n];
+//		Iterator<String> i = s.iterator();
+//		while (i.hasNext()) {
+//			System.out.println(i.next() + "Inserisci parola");
+//		}
+
 		String[] s = new String[n];
 		for (int i = 0; i < n; i++) {
 			System.out.print("Inserisci la parola");
 			s[i] = in.next();
 		}
-		Set<String> setParole = new HashSet<>(); // set distinte
-		Set<String> setParoleDuplicate = new HashSet<>(); // set duplicate
 		for (String parola : s) {
 			if (setParole.contains(parola)) {
 				setParoleDuplicate.add(parola);
